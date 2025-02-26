d218ecd7bad778d6297a069112d02df7675244a3
Rongcui Dong <rongcuid@outlook.com>
2025-02-26 12:45:59 -0500

    Command line fix

--- src/instrGen/instrGen.py
+++ src/instrGen/instrGen.py
@@ -3,26 +3,66 @@ import subprocess
 import os
 import sys
 
+
 def gnerate(input_file, output_file):
     output_suffix = os.path.splitext(output_file)[1]
 
-    if output_suffix == '.ll':
-        result = subprocess.run(['clang', '-fprofile-generate', '-emit-llvm', '-S', *input_file, '-o', output_file], stdout=sys.stdout, stderr=sys.stderr)
+    if output_suffix == ".ll":
+        result = subprocess.run(
+            [
+                "clang",
+                "-fprofile-generate",
+                "-emit-llvm",
+                "-S",
+                *input_file,
+                "-o",
+                output_file,
+            ],
+            stdout=sys.stdout,
+            stderr=sys.stderr,
+        )
 
-    elif output_suffix == '':
-        result = subprocess.run(['clang', '-fprofile-generate', *input_file, '-o', output_file], stdout=sys.stdout, stderr=sys.stderr)
-        
-    elif output_suffix == '.o':
-        result = subprocess.run(['clang', '-fprofile-generate', '-c', *input_file, '-o', output_file], stdout=sys.stdout, stderr=sys.stderr)
+    elif output_suffix == "":
+        result = subprocess.run(
+            ["clang", "-fprofile-generate", *input_file, "-o", output_file],
+            stdout=sys.stdout,
+            stderr=sys.stderr,
+        )
 
-    elif output_suffix == '.so':
-        result = subprocess.run(['clang', '-fprofile-generate', '-shared', *input_file, '-o', output_file], stdout=sys.stdout, stderr=sys.stderr)
+    elif output_suffix == ".o":
+        result = subprocess.run(
+            ["clang", "-fprofile-generate", "-c", *input_file, "-o", output_file],
+            stdout=sys.stdout,
+            stderr=sys.stderr,
+        )
+
+    elif output_suffix == ".so":
+        result = subprocess.run(
+            [
+                "clang",
+                "-fprofile-generate",
+                "-fPIC",
+                "-shared",
+                *input_file,
+                "-o",
+                output_file,
+            ],
+            stdout=sys.stdout,
+            stderr=sys.stderr,
+        )
+
 
 def main():
-    
-    parser = argparse.ArgumentParser(description='Generate LLVM PGO instrumentation code for a given LLVM IR file')
-    parser.add_argument('input', nargs='+', help='Input LLVM IR file(s)')
-    parser.add_argument('output',default='instr.ll', type=str, help='Output .ll or .o or .so file, default to instr.ll')
+    parser = argparse.ArgumentParser(
+        description="Generate LLVM PGO instrumentation code for a given LLVM IR file"
+    )
+    parser.add_argument("input", nargs="+", help="Input LLVM IR file(s)")
+    parser.add_argument(
+        "output",
+        default="instr.ll",
+        type=str,
+        help="Output .ll or .o or .so file, default to instr.ll",
+    )
 
     args = parser.parse_args()
 
